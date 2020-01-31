4f383b5680c147cf8938b2e663dccb4e49f17f54
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-02-01 00:17:37 +0200

    Remove mock static lib

--- library.cpp
+++
@@ -1,7 +0,0 @@
-#include "library.h"
-
-#include <iostream>
-
-void hello() {
-    std::cout << "Hello, World from library!" << std::endl;
-}
--- library.h
+++
@@ -1,6 +0,0 @@
-#ifndef CPP_3_LIBRARY_H
-#define CPP_3_LIBRARY_H
-
-void hello();
-
-#endif //CPP_3_LIBRARY_H
